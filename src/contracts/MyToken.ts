5a6c37e603772be5bb0d2f299996636bd5f6e218
Aleksey <37046107+asiris74@users.noreply.github.com>
2025-07-31 08:02:07 +0400

    Update MyToken.ts

--- src/contracts/MyToken.ts
+++ src/contracts/MyToken.ts
@@ -23,8 +23,8 @@ export class MyToken extends DeployableOP_20 {
     public override onDeployment(_calldata: Calldata): void {
         const maxSupply: u256 = u256.fromString('1000000000000000000000000000'); // Your max supply. (Here, 1 billion tokens)
         const decimals: u8 = 18; // Your decimals.
-        const name: string = 'SoulGoodmanCoin'; // Your token name.
-        const symbol: string = 'SGC'; // Your token symbol.
+        const name: string = 'SoulGoodmanFu'; // Your token name.
+        const symbol: string = 'SGF'; // Your token symbol.
 
         this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
 
